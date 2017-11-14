9fe5cdd8676d7dddf69c8a92231ebbed59cf79a1
Etienne Villedieu <EtienneV@users.noreply.github.com>
2017-11-14 13:58:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-var DHT_IOT = require('../index')
+var DHT_IOT = require('dht-iot')
 var keypair = {publicKey:Buffer.from("6ef441733b1510cc5c93b7201e256b1d9605825e4ea0caca8e202193be090da4", 'hex'),
       secretKey:Buffer.from("6809e0b94f41b1d72cbe29e433e488e1682c703f0ea6a2b5e8bb8a2e74bbf16811759cd5477a8f10b4d73e1f76aaaec81ea201d60ad0358e749c4d3426f5901a", 'hex')}
 var dht_iot = new DHT_IOT({keypair: keypair})
