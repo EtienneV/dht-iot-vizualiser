85efcc6159ec2041d9697d4b606469d997884325
jonrichard <33660080+jonrichard@users.noreply.github.com>
2017-11-20 16:46:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,25 +11,26 @@ app.use(bodyParser.json())
 
 var DHT_IOT = require('dht-iot')
 var keypair = {publicKey:Buffer.from("6ef441733b1510cc5c93b7201e256b1d9605825e4ea0caca8e202193be090da4", 'hex'),
-      secretKey:Buffer.from("6809e0b94f41b1d72cbe29e433e488e1682c703f0ea6a2b5e8bb8a2e74bbf16811759cd5477a8f10b4d73e1f76aaaec81ea201d60ad0358e749c4d3426f5901a", 'hex')}
+secretKey:Buffer.from("6809e0b94f41b1d72cbe29e433e488e1682c703f0ea6a2b5e8bb8a2e74bbf16811759cd5477a8f10b4d73e1f76aaaec81ea201d60ad0358e749c4d3426f5901a", 'hex')}
 var dht_iot = new DHT_IOT({keypair: keypair})
 
 /**
  * Render engine configuration
  */
-app.set('views', __dirname + '/');
-app.engine('ejs', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+ app.set('views', __dirname + '/');
+ app.engine('ejs', require('ejs').renderFile);
+ app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/'));
+ app.use(express.static(__dirname + '/'));
 
 // Spin up the server
 var server = app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+  console.log('running on port', app.get('port'))
 })
 
 var io = require('socket.io').listen(server);
 
+var lat;
 
 /**
 * Routes
@@ -37,9 +38,8 @@ var io = require('socket.io').listen(server);
 
 // Racine
 app.get('/', function (req, res) {
-    res.render('page_racine', {});
+  res.render('page_racine', {});
 })
-
 
 io.on('connection', function(socket){
   console.log('A user connected');
@@ -47,12 +47,17 @@ io.on('connection', function(socket){
   dht_iot.get_notified()
 
   dht_iot.on('new_value', function (hash, data) {
-  	console.log(data.value+" - "+data.timestamp)
-  	socket.emit('new_temp', {
-  		time: data.timestamp,
-  		temp: data.value
-  	});
-  })
+    lat = 0;
+    console.log(data.value+" - "+data.timestamp)
+    if (Date.now()/1000 - time <= 60)
+    {
+     socket.emit('new_temp', {
+      time: data.timestamp,
+      temp: data.value
+    });
+     lat = 1;
+   }
+ })
 
 
   dht_iot.on('get_nodes', function (nodes) {
@@ -64,20 +69,22 @@ io.on('connection', function(socket){
       var has_value = 0
       if(nodes[i].message.r.v) has_value = 1
 
-      geo_node(nodes[i].node.address, has_value).then(function (res){
+        geo_node(nodes[i].node.address, has_value).then(function (res){
 
-        console.log(res.geo.lat+", "+res.geo.lon)
+          console.log(res.geo.lat+", "+res.geo.lon)
 
         //var distance = distance(message.r.id.toString('hex'), infoHash)
 
         //console.log(distance)
-
-        socket.emit('new_node', {
-          long: res.geo.lon,
-          lat: res.geo.lat,
+        if (lat = 1)
+        {
+          socket.emit('new_node', {
+            long: res.geo.lon,
+            lat: res.geo.lat,
           has_value: res.has_value/*,
           distance: distance*/
         });
+        }
       })
 
     }
@@ -95,11 +102,11 @@ function geo_node(ip, has_value) {
       nb_requetes++;
       request('http://ip-api.com/json/'+ip, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-           var donnees = JSON.parse(body);
-           resolve({geo: donnees, has_value: has_value})
-        }
-        reject(error)
-      })
+         var donnees = JSON.parse(body);
+         resolve({geo: donnees, has_value: has_value})
+       }
+       reject(error)
+     })
     }
     else {
       if((Math.floor(new Date() / 1000) - old_date) > 60)
@@ -120,9 +127,9 @@ function distance(firstId, secondId) {
   var min = Math.min(firstId.length, secondId.length)
   var max = Math.max(firstId.length, secondId.length)
   for (var i = 0; i < min; ++i) distance = distance * 256 + (firstId[i] ^ secondId[i])
-  for (; i < max; ++i) distance = distance * 256 + 255
+    for (; i < max; ++i) distance = distance * 256 + 255
 
-  distance = distance / Math.pow(10, 80)
+      distance = distance / Math.pow(10, 80)
 
-  return distance
-}
+    return distance
+  }
